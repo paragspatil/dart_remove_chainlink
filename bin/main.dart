a77ef8f8804393a76d09baff34a21afb6a0b192f
Parag <paragspatil123@gmail.com>
2019-10-04 06:53:29 +0530

    removeLink com=leted

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,15 @@
-// https://s1.whiteboardfox.com/s/2ae37dc832647bad.png
-class Link {
+// https://s1.whiteboardfox.com/s/2ae37dc8326
+import 'dart:io';
+import 'dart:convert';
+
+class Link{
   String value;
   Link next;
 
   Link(this.value);
 }
 
-main() {
+void main() {
   //Implementation of the given diagram https://s1.whiteboardfox.com/s/2ae37dc832647bad.png
   Link firstLink = Link('Raj');
   Link secondLink = Link('Makiko');
@@ -24,10 +27,11 @@ main() {
   Link sixthLink = Link('Parvati');
   fifthLink.next = sixthLink;
 
-  print(firstLink);
+  printChain(firstLink);
   print('-------');
   removeLink(firstLink, 'Makiko');
-  print(firstLink);
+
+  // print(firstLink);
 }
 
 // Printing the linkTree
@@ -39,8 +43,40 @@ void printChain(Link link) {
   }
 }
 
+
 // Exercise: Write a function which takes in a link and a word. It should remove the link with value matching word
 void removeLink(Link firstLink, String word) {
   // Implement function here
+  int cont = 1;
+  int i = 0;
+  Link Temp = firstLink;
+  Link Temp2 = firstLink;
 
+
+  while(Temp!= null){
+    if(Temp.value == word){
+      i = cont;
+
+
+    }
+    Temp = Temp.next;
+    cont = cont + 1;
+  }
+
+  while(i!=2){
+    Temp2 = Temp2.next;
+    i = i-1;
+  }
+  Link Temp3 = Temp2.next;
+  Temp2.next = Temp3.next;
+
+  Link currLink = Temp2;
+  print('---------');
+  while (currLink != null) {
+
+    print(currLink.value);
+    currLink = currLink.next;
+  }
 }
+
+
